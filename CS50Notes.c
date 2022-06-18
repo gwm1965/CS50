302269ed0004362a7311bf1dab078326dd1097d6
Mario Wilson Garcia <70682960+gwm1965@users.noreply.github.com>
2022-06-18 05:31:41 -0300

    Update CS50Notes.c

--- CS50Notes.c
+++ CS50Notes.c
@@ -71,3 +71,98 @@ int main(void)
 }
 
 1:32:44 17/06/2022
+
+#include <cs50.h>
+#include <stdio.h>
+
+//Truncation
+
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+    //get number from users
+ int x = get_int("x: ");
+ int y = get_int("y: ");
+ // divided x by y   
+ float z = (float) x /(float) y;
+ printf("%f\n", z);
+}
+
+//Conditions and compare operators
+int main (void)
+{
+    //Prompt user for x
+    int x = get_int("x: ");
+    
+    //prompt user for y
+    int y = get_int("y: ");
+    
+    //compare x and y
+    if(x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if(x > y) 
+    {
+        printf("x is greater than y\n");
+        
+    }
+    else {
+        printf("x is equal than y\n");
+    }
+}
+
+//Agree
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c = get_char("Do you agree? ");
+    if(c == 'y' || c == 'Y')
+    {
+        printf("Agreed\n");
+    }
+    else if(c == 'n' || c == 'N')
+    {
+        printf("not agree\n");
+    }
+}
+
+
+
+}
+
+//While_++1
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+int i = 0;
+
+while(i < 50) 
+{
+    printf("Hello, World");
+    i++;
+}
+}
+
+//While_--1
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+int i = 50;
+
+while(i > 0) 
+{
+    printf("Hello, World");
+    i--;
+}
+}
